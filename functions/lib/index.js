0c0af48fc6d8d91eef9e140ee7fe092797723200
Eduardo Mohedano <emohedanomonter@gmail.com>
2018-10-07 23:52:46 -0500

    Added support to add expenses & list categories

---
+++ functions/lib/index.js
@@ -0,0 +1,114 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const admin = require("firebase-admin");
+const functions = require("firebase-functions");
+const actions_on_google_1 = require("actions-on-google");
+// // Start writing Firebase Functions
+// // https://firebase.google.com/docs/functions/typescript
+//
+// export const helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
+// Instantiate a firestore client
+admin.initializeApp(functions.config().firebase);
+const db = admin.firestore();
+db.settings({ timestampsInSnapshots: true });
+const app = actions_on_google_1.dialogflow({ debug: true });
+//Setup contexts
+const Contexts = {
+    ADD_EXPENSE: 'add_expense_context'
+};
+app.intent('Default Fallback Intent', conv => {
+    conv.ask(`I didn't understand`);
+    conv.ask(`I'm sorry, can you try again?`);
+});
+//TODO: add sign in
+//conv.ask(new SignIn());
+const EN_TRANSLATIONS = {
+    EXPENSE_ADDED_MESSAGE: 'Expense added',
+    CATEGORY_ADDED_MESSAGE: 'Category added',
+    CREATE_CATEGORY_AND_SAVE_EXPENSE_MESSAGE: 'This category does not exist, would you like to create it',
+    EMPTY_CATEGORIES_MESSAGE: 'There are no categories yet',
+    CATEGORIES_LIST_MESSAGE: 'These categores are avaliable:'
+};
+const TRANSLATIONS = EN_TRANSLATIONS;
+const OTHER_CATEGORY = 'other';
+app.intent('add_expense', (conv, { expense }) => __awaiter(this, void 0, void 0, function* () {
+    conv.contexts.set(Contexts.ADD_EXPENSE, 5);
+    const allCategories = yield getCategories();
+    const expenseCategory = expense['expense_category'];
+    const expenseModel = expenseFromUseInput(expense);
+    const categoryExists = allCategories.indexOf(expenseCategory) !== -1;
+    if (!categoryExists) {
+        conv.data['inexistentCategory'] = expenseCategory;
+        conv.data['expenseToCreate'] = expenseModel;
+        conv.ask(TRANSLATIONS.CREATE_CATEGORY_AND_SAVE_EXPENSE_MESSAGE);
+        return conv;
+    }
+    yield createExpense(expenseModel);
+    conv.add(TRANSLATIONS.EXPENSE_ADDED_MESSAGE);
+    return conv;
+}));
+app.intent('create_category_on_add_expense_yes', (conv) => __awaiter(this, void 0, void 0, function* () {
+    yield createCategory(conv.data['inexistentCategory']);
+    yield createExpense(conv.data['expenseToCreate']);
+    conv.add(TRANSLATIONS.CATEGORY_ADDED_MESSAGE);
+    return conv;
+}));
+app.intent('create_category_on_add_expense_no', (conv) => __awaiter(this, void 0, void 0, function* () {
+    const expenseCategory = conv.data['inexistentCategory'];
+    const expenseToCreate = conv.data['expenseToCreate'];
+    expenseToCreate.category = OTHER_CATEGORY;
+    yield createCategory(expenseCategory);
+    yield createExpense(expenseToCreate);
+    conv.add(TRANSLATIONS.CATEGORY_ADDED_MESSAGE);
+    return conv;
+}));
+app.intent('list_expense_categories', (conv) => __awaiter(this, void 0, void 0, function* () {
+    const categories = yield getCategories();
+    if (categories.length === 0) {
+        conv.add(TRANSLATIONS.EMPTY_CATEGORIES_MESSAGE);
+        return conv;
+    }
+    conv.add(`${TRANSLATIONS.CATEGORIES_LIST_MESSAGE} ${categories.join(', ')}`);
+    return conv;
+}));
+function getCategories() {
+    const categoriesRef = db.collection('expense_categories');
+    return categoriesRef.get().then((snapshot) => {
+        let results = [];
+        if (snapshot.docs.length === 0) {
+            return results;
+        }
+        snapshot.forEach((doc) => {
+            results.push(doc.data());
+        });
+        results = results.map(r => r.name);
+        return results;
+    });
+}
+function createCategory(categoryName) {
+    return db.collection('expense_categories').add({ name: categoryName });
+}
+function createExpense(expenseModel) {
+    return db.collection('expenses').add(expenseModel);
+}
+function expenseFromUseInput(expenseEntity) {
+    return {
+        amount: expenseEntity['unit-currency']['amount'],
+        currency: expenseEntity['unit-currency']['currency'],
+        category: expenseEntity['expense_category'] || OTHER_CATEGORY,
+        creationDate: new Date()
+    };
+}
+// HTTP Cloud Function for Firebase handler
+exports.dialogflowFirebaseFulfillment = functions.https.onRequest(app);
+//# sourceMappingURL=index.js.map
